7b7942ec849034ce0dbf5b15d32aecf65615598e
Bastien <B.prigent@rubika-edu.com>
2021-05-15 12:54:28 +0200

    add jump + gravity

--- Spyro le Dragon/Assets/Script/SpyroController.cs
+++ Spyro le Dragon/Assets/Script/SpyroController.cs
@@ -8,24 +8,60 @@ public class SpyroController : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jump = 3f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+    public bool isGrounded;
     private void Update()
+    {
+        Move();
+        Gravity();
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    public void Move()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; 
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+    public void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jump * -2f * gravity);
     }
 }
